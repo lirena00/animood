fe80e4075c0514ff345d6df9d8bed2a1754efd79
lirena00 <sakshamkushwaha12776@gmail.com>
2025-07-23 19:41:53 +0530

    feat: update configuration and enhance document head with favicon and preconnect links

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,50 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+
+        {/* Preconnect to external domains */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link rel="preconnect" href="https://graphql.anilist.co" />
+
+        {/* DNS prefetch for performance */}
+        <link
+          rel="dns-prefetch"
+          href="https://analytics-eta-green.vercel.app"
+        />
+
+        {/* Web App Manifest */}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="msapplication-TileColor" content="#23A9D5" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+      </Head>
       <body>
         <Main />
         <NextScript />
